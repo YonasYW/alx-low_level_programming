1e22c71bb3a9f0ba683b82a0ee175b9f2ad78407
Yonas yalew <yonasyalewwerke@gmail.com>
2023-10-06 06:58:06 -0700

    alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print the letters of the alphabet
+ *
+ * Description: print the letters of the alphabet except e, q
+ *
+ * Return: Always 0 (Success)
+ */
+
+	int main(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'q' && letter != 'e')
+			putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
