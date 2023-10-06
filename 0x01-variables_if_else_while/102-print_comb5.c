5e41d6a8a3be82f197f31443e4663b3356cb4d2f
Yonas yalew <yonasyalewwerke@gmail.com>
2023-10-06 10:02:10 -0700

    print comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - print pairs of double digit combos
+ *
+ * Description: print pairs of double digit combos
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i, j;
+
+	i = 0;
+	while (i < 100)
+	{
+	j = i;
+	while (j < 100)
+		{
+		putchar(i / 10 + '0');
+		putchar(i % 10 + '0');
+
+		putchar(' ');
+
+		putchar(j / 10 + '0');
+		putchar(j % 10 + '0');
+
+		if (i != 99 || j != 99)
+			{
+			putchar(',');
+			putchar(' ');
+			}
+
+		j++;
+		}
+	i++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
