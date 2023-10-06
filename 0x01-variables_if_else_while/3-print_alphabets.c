9d1e39b7fd5c8de2d1a89ee212284a075bd3443f
Yonas yalew <yonasyalewwerke@gmail.com>
2023-10-06 06:44:43 -0700

    alphABETS

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,11 +12,9 @@ int main(void)
 {
 	char letter;
 
-	// Print lowercase alphabet
 	for (letter = 'a'; letter <= 'z'; letter++)
 	putchar(letter);
 
-	// Print uppercase alphabet
 	for (letter = 'A'; letter <= 'Z'; letter++)
 	putchar(letter);
 
