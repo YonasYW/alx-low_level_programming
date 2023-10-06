a0d3e158a0bd60344641ec8af7d899148fc033ec
Yonas yalew <yonasyalewwerke@gmail.com>
2023-10-06 06:20:40 -0700

    prints alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,11 +10,12 @@
 
 int main(void)
 {
-    char letter;
+char letter;
 
-    for (letter = 'a'; letter <= 'z'; letter++) putchar(letter);
+for (letter = 'a'; letter <= 'z'; letter++)
+       	putchar(letter);
 
     putchar('\n');
 
-    return 0;
+return (0);
 }
