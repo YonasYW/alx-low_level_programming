00ca2af7795a1f45c0811dad903ea1acf29a6f38
Yonas yalew <yonasyalewwerke@gmail.com>
2023-10-06 07:49:18 -0700

    prints base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,7 +3,7 @@
 /**
  * main - print the letters of the alphabet
  *
- * Description: prints all the numbers of base 16 in lowercase, followed by a new line
+ * Description: prints all the numbers of base 16 in lowercase.
  *
  * Return: Always 0 (Success)
  */
@@ -14,7 +14,11 @@ int i = 0;
 
 	while (i < 16)
 	{
+		if (i < 10) {
 	putchar('0' + i);
+	} else {
+		putchar('a' + i - 10);
+    }
 	i++;
 	}
 
