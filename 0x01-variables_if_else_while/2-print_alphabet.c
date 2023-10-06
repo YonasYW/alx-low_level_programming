4fe9f217c934fb21e850d9f6e7d0def896990ef3
Yonas yalew <yonasyalewwerke@gmail.com>
2023-10-06 06:10:27 -0700

    prints alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print the letters of the alphabet
+ *
+ * Description: print the letters of the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main() {
+    char letter;
+
+    for (letter = 'a'; letter <= 'z'; letter++) {
+        putchar(letter);
+    }
+
+    putchar('\n');
+
+    return 0;
+}
