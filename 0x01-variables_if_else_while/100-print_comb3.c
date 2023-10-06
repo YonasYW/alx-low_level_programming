e80a5b92a3239a8bd2a97c82505a2577cce4b6c3
Yonas yalew <yonasyalewwerke@gmail.com>
2023-10-06 09:00:50 -0700

    print comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - print double digit combos
+ *
+ * Description: print double digit combos
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+    int i, j;
+
+    i = 0;
+    j = 1;
+
+    while (i < 9) {
+        while (j <= 9) {
+            putchar(i + '0');
+            putchar(j + '0');
+
+            if (i != 8 || j != 9) {
+                putchar(',');
+                putchar(' ');
+            }
+
+            j++;
+        }
+
+        i++;
+        j = i + 1;
+    }
+
+    putchar('\n');
+
+    return 0;
+}
