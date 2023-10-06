da7a1a4cc4c8301409c037b182b9cb53789b862e
Yonas yalew <yonasyalewwerke@gmail.com>
2023-10-06 10:20:12 -0700

    print comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,25 +15,25 @@ int i, j;
 	i = 0;
 	while (i < 100)
 		{
-		j = i + 1;
-		while (j < 100)
-			{
-				putchar(i / 10 + '0');
-				putchar(i % 10 + '0');
+			j = i + 1;
+			while (j < 100)
+				{
+					putchar(i / 10 + '0');
+					putchar(i % 10 + '0');
 
-				putchar(' ');
+					putchar(' ');
 
-				putchar(j / 10 + '0');
-				putchar(j % 10 + '0');
+					putchar(j / 10 + '0');
+					putchar(j % 10 + '0');
 
-				if (i != 99 || j != 99)
-					{
-						putchar(',');
-						putchar(' ');
-					}
+					if (i != 99 || j != 99)
+						{
+							putchar(',');
+							putchar(' ');
+						}
 
-			j++;
-			}
+				j++;
+				}
 		i++;
 		}
 
