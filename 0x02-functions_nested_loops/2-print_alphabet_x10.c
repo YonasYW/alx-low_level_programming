029743f410fc09025d5287594aa72ebd40166d05
Yonas yalew <yonasyalewwerke@gmail.com>
2023-10-10 01:34:39 -0700

    alphabet x 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char letter = 'a';
+	int count = 0;
+
+	while (count < 10)
+       	{
+		letter = 'a';
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+
+		count++;
+	}
+}
