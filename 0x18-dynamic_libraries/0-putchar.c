bf1d66178229254301dd749d6dd00b1d0a1d85f5
Yonas yalew <yonasyalewwerke@gmail.com>
2024-01-15 00:12:11 -0800

    dynamic libs

---
+++ 0x18-dynamic_libraries/0-putchar.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+/**
+ * main - prints _putchar
+ *
+ * _putchar - writes the character c to stdout
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int main(void)
+{
+	const char *message = "_putchar\n";
+
+	write(1, message, sizeof("_putchar\n") - 1);
+
+	return (0);
+}
