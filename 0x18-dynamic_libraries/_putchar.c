402ac86eec6e8ca8a5cf25a83e7d277c00367edc
Yonas yalew <yonasyalewwerke@gmail.com>
2024-01-15 01:08:40 -0800

    d l

--- 0x18-dynamic_libraries/_putchar.c
+++ 0x18-dynamic_libraries/_putchar.c
@@ -1,19 +1,13 @@
 #include <unistd.h>
 
 /**
- * main - prints _putchar
- *
  * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-
-int main(void)
+int _putchar(char c)
 {
-	const char *message = "_putchar\n";
-
-	write(1, message, sizeof("_putchar\n") - 1);
-
-	return (0);
+	return (write(1, &c, 1));
 }
